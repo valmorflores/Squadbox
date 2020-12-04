17000947cca432ab3c2a1e0a74446e4d3dea0bb1
valmorflores <valmorflores@gmail.com>
2020-12-04 10:36:05 -0300

    Example

---
+++ lib/config.example.dart
@@ -0,0 +1,4 @@
+const String ADMOB_APP_ID = "ca-app-pub-0000000000000000~0000000000";
+const String ADMOB_TEST_DEVICE =
+    'test_device_id - run ad then check device logs for value';
+const String ADMOB_UNIT_ID = "ca-app-pub-0000000000000000/0000000000";
